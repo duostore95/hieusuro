99ab9b7920e0cacdf8be87c462291d8287d74a21
duostore95 <duostore95@gmail.com>
2025-10-29 17:45:26 +0700

    Update storage.ts

--- server/storage.ts
+++ server/storage.ts
@@ -256,7 +256,7 @@ export class MemStorage implements IStorage {
         price: "1990000",
         imageUrl: "/attached_assets/hieu-suro-profile.png",
         duration: "8 tuần",
-        studentCount: 150,
+        studentCount: 922,
         rating: "4.9",
         badge: "Bestseller",
         status: "active",
